25891bb659683f7f373585d665d2bff8e5a90932
David Stewart <david@dstewart101.com>
2019-05-26 23:06:16 +0100

    list of heroes with editing when one is selected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -8,14 +8,18 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = 
-  {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
   heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void 
+  {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
